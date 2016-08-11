bfd3615a412686e5e3d99c6ac6428c5ff84ec46a
hcpss-vincevince1006 <vincent_dubay@hcpss.org>
2016-08-11 13:48:14 -0400

    forgot commas and parentheses

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ var $fadeScroll = function(i) {
 		    'transition' : 'opacity 0.5s'			
 			})
 			$('.jumbo-nav--fixed').css({
-				'background-color': 'rgba(87, 87, 87, 1)',
+			'background-color': 'rgba(87, 87, 87, 1)',
 		    '-webkit-transition' : 'background-color 0.5s',
 		    '-moz-transition' : 'background-color 0.5s',
 		    'transition' : 'background-color 0.5s',	
-	    	'box-shadow': '0 9px 39px -31px rgba(0,0,0,.34)',
+	    	'box-shadow': '0 9px 39px -31px rgba(0,0,0,.34)'
 			})
 		}
 		else {
 		$('.jumbo-nav--fixed').css({
-			'background-color' : 'rgba(82, 82, 82, 0.4)',
+		'background-color' : 'rgba(82, 82, 82, 0.4)',
 	    '-webkit-transition' : 'background-color 0.5s',
 	    '-moz-transition' : 'background-color 0.5s',
 	    'transition' : 'background-color 0.5s',
 	    'box-shadow' : 'initial'
 		})	
 		$('.fixed-chevron-scroll').css({
-			'opacity' : '1',
+		'opacity' : '1',
 	    '-webkit-transition' : 'opacity 0.5s',
 	    '-moz-transition' : 'opacity 0.5s',
 	    'transition' : 'opacity 0.5s'			
@@ -41,7 +41,7 @@ var $fadeScroll = function(i) {
 
 function checkSize() {
 	if (window.matchMedia("(max-width: 700px)").matches) {
-		$('.jumbo-nav--fixed').css({'background-color' : 'rgba(82, 82, 82, 1'})
+		$('.jumbo-nav--fixed').css({'background-color' : 'rgba(82, 82, 82, 1)'})
 		$(window).off("scroll", $fadeScroll);
 	} else {
 		$('.jumbo-nav--fixed').css({'background-color' : 'rgba(82, 82, 82, 0.4)'})
